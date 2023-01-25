5d2c08c1667e27311638934a1d7a5dd888dcbf03
André Nogueira <aanogueira@protonmail.com>
2023-01-25 19:31:22 +0000

    Testing script

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ kubectl config set-credentials actions-runner --token $4
 kubectl config set-context local --cluster local --user actions-runner --namespace default
 kubectl config use-context local
 
-echo "{diff}={$(\
-  for var in $(kubectl kustomize --context local $1 | grep '{' | awk -F"[{}]" '{print$2}'); do \
+echo "diff<<EOF" >> $GITHUB_OUTPUT
+echo "$(for var in $(kubectl kustomize --context local $1 | grep '{' | awk -F"[{}]" '{print$2}'); do \
     unset $var && export $var=$(\
       kubectl get --context local cm cluster-values -n flux-system -o yaml | \
       grep $var | \
@@ -16,4 +16,5 @@ echo "{diff}={$(\
     done; \
   kubectl kustomize --context local $1 | envsubst | \
   kubectl diff --context local -f -\
-)}" >> $GITHUB_OUTPUT
+)" >> $GITHUB_OUTPUT
+echo "EOF" >> $GITHUB_OUTPUT
