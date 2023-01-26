81528528ca936bb4c35e3d1702375db62194eb1d
André Nogueira <aanogueira@protonmail.com>
2023-01-26 11:41:44 +0000

    Testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,15 +23,28 @@
 # kubectl config set-context local --cluster local --user actions-runner --namespace default
 # kubectl config use-context local
 
+# echo "diff<<EOF" >> $GITHUB_OUTPUT
+# echo "$(for var in $(kubectl kustomize --context local $1 | grep -o '{[^}]*}' | awk -F"[{}]" '{print$2}'); do \
+#     unset $var && export $var=$(\
+#       kubectl get --context local cm cluster-values -n flux-system -o yaml | \
+#       grep $var | \
+#       awk '{sub(/:/," ");$1=$1;print $2}' | \
+#       tr -d " "); \
+#     done; \
+#   kubectl kustomize --context local $1 | envsubst | \
+#   kubectl diff --context local -f -\
+# )" >> $GITHUB_OUTPUT
+# echo "EOF" >> $GITHUB_OUTPUT
+
 echo "diff<<EOF" >> $GITHUB_OUTPUT
-echo "$(for var in $(kubectl kustomize --context local $1 | grep -o '{[^}]*}' | awk -F"[{}]" '{print$2}'); do \
+echo "$(for var in $(kubectl kustomize | grep -o '{[^}]*}' | awk -F"[{}]" '{print$2}'); do \
     unset $var && export $var=$(\
       kubectl get --context local cm cluster-values -n flux-system -o yaml | \
       grep $var | \
       awk '{sub(/:/," ");$1=$1;print $2}' | \
       tr -d " "); \
     done; \
-  kubectl kustomize --context local $1 | envsubst | \
+  kubectl kustomize | envsubst | \
   kubectl diff --context local -f -\
 )" >> $GITHUB_OUTPUT
 echo "EOF" >> $GITHUB_OUTPUT
