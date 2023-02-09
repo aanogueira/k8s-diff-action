87ae3f154dca7c0b5a2f926034d0f8c4235c0c4e
André Nogueira <aanogueira@protonmail.com>
2023-02-09 18:42:12 +0000

    Testing ci

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,22 +12,19 @@ done
 echo "INFO - Validating clusters"
 find ./clusters -maxdepth 2 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
   do
-    kubeval ${file} --strict --ignore-missing-schemas --additional-schema-locations=file:///tmp/flux-crd-schemas
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
+    kubeval $file --strict --ignore-missing-schemas --additional-schema-locations=file:///tmp/flux-crd-schemas
+    if [ $PIPESTATUS[0] != 0 ]; then
       exit 1
     fi
 done
 
-kustomize_flags="--load-restrictor=LoadRestrictionsNone --reorder=legacy"
-kustomize_config="kustomization.yaml"
-
 echo "INFO - Validating kustomize overlays"
-find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
+find . -type f -name kustomization.yaml -print0 | while IFS= read -r -d $'\0' file;
   do
-    echo "INFO - Validating kustomization ${file/%$kustomize_config}"
-    kustomize build "${file/%$kustomize_config}" $kustomize_flags | \
+    echo "INFO - Validating kustomization $(dirname $file)"
+    kustomize build $(dirname $file) | \
       kubeval --ignore-missing-schemas --strict --additional-schema-locations=file:///tmp/flux-crd-schemas
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
+    if [ $PIPESTATUS[0] -ne 0 ]; then
       exit 1
     fi
 done
