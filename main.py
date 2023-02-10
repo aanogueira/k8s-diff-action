fdb8940d7783760045aafbee61305622c6cc347a
André Nogueira <aanogueira@protonmail.com>
2023-02-10 18:04:17 +0000

    Testin diff fix

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def add_helm_repo(chart_repo, chart_url, secret=None):
 def get_helm_diff(name, chart_repo, chart_name, chart_version, namespace, values):
     values_command = ['echo', values]
     substr_command = "perl -pe 's{(?|\$\{([_a-zA-Z]\w*)\}|\$([_a-zA-Z]\w*))}{$ENV{$1}//$&}ge'"
-    release_command = f"helm template name {chart_repo}/{chart_name} --version chart_version -n namespace --skip-tests --no-hooks -f -"
+    release_command = f"helm template {name} {chart_repo}/{chart_name} --version {chart_version} -n {namespace} --skip-tests --no-hooks -f -"
     diff_command = "kubectl diff --server-side=false -f -"
 
     values = subprocess.Popen(values_command, stdout=subprocess.PIPE)
